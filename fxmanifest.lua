7052bcfb627b5f1b6032334a9d220c24c11493ea
v1b3zz-g3 <158081030+v1b3zz-g3@users.noreply.github.com>
2026-03-04 16:11:44 +0200

    push

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Glowie / sf_blackmarket'
+description 'SF Black Market Deliveries'
+version '2.0'
+
+shared_script "config.lua"
+
+client_scripts {
+  "@PolyZone/client.lua",
+  "client/**/*",
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    "server/**/*",
+}
+
+ui_page 'web/build/index.html'
+
+files {
+    'web/build/index.html',
+    'web/build/**/*',
+}
+
+lua54 'yes'
